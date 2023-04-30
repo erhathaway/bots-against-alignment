e46949b55e8b64adcc35f778c462ac20f77ac1dc
Ethan Hathaway <erhathaway@gmail.com>
2023-04-30 11:43:55 -0700

    Fix: slowapi bug

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -22,7 +22,7 @@ from slowapi.errors import RateLimitExceeded
 
 app = FastAPI()
 
-limiter = Limiter(key_func=get_remote_address, default_limits=["60/minute"])
+limiter = Limiter(key_func=get_remote_address, default_limits=["60/minute"], auto_check=False, enabled=True)
 
 
 origins = ["*"]
