09995201f8caf4fde7c8f805aee9918192844a11
Ethan Hathaway <erhathaway@gmail.com>
2026-02-01 20:57:26 -0800

    feat: add Storybook stories for HomePage component

---
+++ app/src/routes/HomePage.stories.svelte
@@ -0,0 +1,11 @@
+<script module lang="ts">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import HomePage from './+page.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Home/HomePage',
+		component: HomePage
+	});
+</script>
+
+<Story name="Default" />
